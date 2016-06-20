846f84553e6a1ac316e929256d1fad16c68ecb86
tjinjin <tjinjinprogram@gmail.com>
2016-06-20 22:47:53 +0900

    Add omnitauth settings

--- rails-app/whitebelg/config/initializers/devise.rb
+++ rails-app/whitebelg/config/initializers/devise.rb
@@ -265,4 +265,6 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], scope: 'email'
+  config.omniauth :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: 'user,public_repo'
 end
