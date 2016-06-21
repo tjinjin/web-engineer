cbffde3c411a7cb7e93187008c0adf394916eb0c
tjinjin <tjinjinprogram@gmail.com>
2016-06-22 00:52:07 +0900

    Userモデルの実装

--- rails-app/whitebelg/app/models/user.rb
+++ rails-app/whitebelg/app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.name = auth.info.name
+      user.token = auth.credentials.token
+      user.password = Devise.frienddly_token[0,20]
+    end
+  end
 end
