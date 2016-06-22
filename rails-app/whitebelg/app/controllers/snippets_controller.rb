e6c2250647da9501beca3df2a64373e2488eb43d
tjinjin <tjinjinprogram@gmail.com>
2016-06-23 01:43:50 +0900

    非公式なところにした。公式はもうないっぽい？

--- rails-app/whitebelg/app/controllers/snippets_controller.rb
+++ rails-app/whitebelg/app/controllers/snippets_controller.rb
@@ -4,7 +4,7 @@ class SnippetsController < ApplicationController
   def index
     @snippets = Snippet.all
     @snippets.each do |snippet|
-      uri = URI.parse('http://pygments.org/')
+      uri = URI.parse('http://pygments.simplabs.com/')
       request = Net::HTTP.post_form(uri, lang: snippet.language, code: snippet.plain_code)
       snippet.update(highlighted_code: request.body)
     end
